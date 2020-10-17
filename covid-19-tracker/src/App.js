901444e91280056ea8c3ef0119db1d128b79e33a
Rezibajra <rezibajjra@gmail.com>
2020-10-17 17:25:37 +0545

    Styles added and firebase hosting

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -11,7 +11,7 @@ import Map from './Map';
 import Table from './Table';
 import LineGraph from './LineGraph';
 import './App.css';
-import { sortData } from './util';
+import { sortData,prettyPrintStat } from './util';
 import "leaflet/dist/leaflet.css";
 
 function App() {
@@ -22,6 +22,7 @@ function App() {
   const [mapCenter,setMapCenter]=useState({lat:34.80746,lng:-40.4796});
   const [mapZoom,setMapZoom]=useState(3);
   const [mapCountries,setMapCountries]=useState([]);
+  const [casesType,setCasesType]=useState("cases");
 
   useEffect(()=>{
     fetch("https://disease.sh/v3/covid-19/all")
@@ -86,11 +87,12 @@ function App() {
       </FormControl>
       </div>
       <div className="app__stats">
-          <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases}/>
-          <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered}/>
-          <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths}/>
+          <InfoBox isRed active={casesType === "cases"} onClick={e=>setCasesType("cases")} title="Coronavirus Cases" cases={prettyPrintStat(countryInfo.todayCases)} total={prettyPrintStat(countryInfo.cases)}/>
+          <InfoBox active={casesType === "recovered"} onClick={e=>setCasesType("recovered")} title="Recovered" cases={prettyPrintStat(countryInfo.todayRecovered)} total={prettyPrintStat(countryInfo.recovered)}/>
+          <InfoBox isRed active={casesType === "deaths"} onClick={e=>setCasesType("deaths")} title="Deaths" cases={prettyPrintStat(countryInfo.todayDeaths)} total={prettyPrintStat(countryInfo.deaths)}/>
       </div>
-      <Map countries={mapCountries} 
+      <Map casesType={casesType}
+      countries={mapCountries} 
       center={mapCenter}
       zoom={mapZoom}/>
       </div>
@@ -98,8 +100,8 @@ function App() {
         <CardContent>
           <h3>Live cases by country</h3>
           <Table countries={tableData}/>
-          <h3>Worldwide new cases</h3>
-          <LineGraph/>
+          <h3 className="app__graphTitle">Worldwide new {casesType}</h3>
+          <LineGraph className="app__graph" casesType={casesType}/>
         </CardContent>
       </Card>
     </div>
--- covid-19-tracker/src/Table.js
+++ covid-19-tracker/src/Table.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import numeral from "numeral";
 import './Table.css';
 function Table({countries}) {
     return (
@@ -6,7 +7,7 @@ function Table({countries}) {
            {countries.map(({country,cases})=>(
                <tr>
                    <td>{country}</td>
-                   <td><strong>{cases}</strong></td>
+                   <td><strong>{numeral(cases).format("0,0a")}</strong></td>
                </tr>
            ))} 
         </div>
